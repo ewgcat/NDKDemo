13d75b393ad1ae9fc5104f4f65619752ce9d7527
lishuaihua <850716183@qq.com>
2021-01-14 17:51:31 +0800

    移除无用代码

--- app/.cxx/cmake/debug/x86/cmake_install.cmake
+++ app/.cxx/cmake/debug/x86/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/lishuaihua/Desktop/ndk-samples-main/hello-jni/app/src/main/cpp
+# Install script for directory: /Users/lishuaihua/Desktop/NDKDemo/app/src/main/cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/lishuaihua/Desktop/ndk-samples-main/hello-jni/app/.cxx/cmake/debug/x86/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/lishuaihua/Desktop/NDKDemo/app/.cxx/cmake/debug/x86/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
